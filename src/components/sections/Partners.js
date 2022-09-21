a1109c6cd56d90539ce3695300864521e1faa76e
Puvichandra <45979027+Puvichandra@users.noreply.github.com>
2022-09-21 13:39:34 +0530

    Add files via upload

---
+++ src/components/sections/Partners.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './partners.css'
+import Pink from '../../assets/pinksale.png'
+import Bsc from '../../assets/bscscan.svg'
+import Pancake from '../../assets/pancake.png'
+import Poocoin from '../../assets/poocoin.png'
+
+const Partners = () => {
+  return (
+    
+    <section class='partners'>
+    {/* <h3>Our Partners</h3> */}
+    <div class='partners-img-container'>
+      <div class='partners-img'>
+        <a href='https://www.pinksale.finance/'>
+            <img src={Pink}></img>
+        </a>
+        <a href='https://bscscan.com/'>
+            <img src={Bsc}></img>
+        </a>
+        <a href='https://pancakeswap.finance/'>
+            <img src={Pancake}></img>
+        </a>
+        <a href='https://poocoin.app/'>
+            <img src={Poocoin}></img>
+        </a>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Partners
